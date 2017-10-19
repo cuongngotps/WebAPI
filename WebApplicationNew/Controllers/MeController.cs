e537c1520542cad9d442ba87aaec0cb0e7c90413
cuongngotps <cuong.ngo@tpssoft.com>
2017-10-19 14:52:22 +0700

    create project

---
+++ WebApplicationNew/Controllers/MeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Owin;
+using WebApplicationNew.Models;
+using AutoMapper;
+
+namespace WebApplicationNew.Controllers
+{
+    [RoutePrefix("api/me")]
+    [LogActionFilter]
+    public class MeController : ApiController
+    {
+        private ApplicationUserManager _userManager;
+
+        public MeController()
+        {
+        }
+
+        public MeController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        // GET api/Me
+        public GetViewModel Get()
+        {
+            var user = UserManager.FindById(User.Identity.GetUserId());
+            return new GetViewModel() { Hometown = user.Hometown };
+        }
+
+        [HttpGet]
+        [Route("users/all")]
+        public IEnumerable<UserViewModel> GetAll()
+        {
+            return Mapper.Map<IEnumerable<UserViewModel>>(UserManager.Users.ToList());
+        }
+
+        [HttpGet]
+        [Route("users/delete/{id}")]
+        //[Authorize(Roles = "admin")]
+        public void DeleteUser(String id)
+        {
+            UserManager.Delete(UserManager.FindById(id));
+        }
+
+        [HttpPost]
+        [Route("users/add")]
+        public String AddNewUser([FromBody]String userName)
+        {
+            return userName;
+        }
+    }
+}
---
+++ WebApplicationNew/Scripts/myScript/myScript.js
@@ -0,0 +1,51 @@
+﻿(function () {
+	var app = angular.module('myApp', []);
+
+	app.controller('myCtrl', ['$http', '$scope', 'addNewUser', function ($http, $scope, addNewUser) {
+
+		$http.get('api/me/users/all')
+			.then(function (response) {
+				$scope.users = response.data;
+			}, function (response) {
+
+			});
+
+		$scope.deleteUser = function (id) {
+
+			$http.get('api/me/users/delete/' + id)
+				.then(function (response) {
+
+				}, function (response) {
+
+				});
+		};
+
+		$scope.addUser = function () {
+			addNewUser.add($scope);
+		}
+	}]);
+
+	app.factory('addNewUser', ['$http', function ($http) {
+
+		return {
+			add:add
+		}
+
+		function add($scope) {
+			$http.post('api/me/users/add', $scope.email)
+				.then(function (response) {
+
+				}, function (response) {
+
+				});
+			$scope.email = "";
+		}
+	}]);
+
+	app.directive('itemUser', function () {
+		return {
+			templateUrl: '/src/item-user.html'
+		}
+	});
+
+})();
